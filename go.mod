832ddccc7617d99b934b8cd0964b486ac818995f
Alex Alexandrovich <javafirst17@gmail.com>
2021-06-27 00:43:17 +0300

    Update struct files

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module github.com/jf17/jaguar
 
 go 1.16
-
-require github.com/jf17/jaguar-dependency-downloader v0.0.2-alpha
--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
-import "github.com/jf17/jaguar-dependency-downloader"
+import "github.com/jf17/jaguar/dependency"
 
 func main() {
-	jagdepdownl.DownloadFromPom("", "")
+	downloader.DownloadFromPom("", "")
 }
